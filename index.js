6790d230e78da9ebfa63fb135ae4166ae3ecd1ea
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2017-05-16 15:38:25 +0200

    feat(JS): Add javascript build scaffolding

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+const logger = require('./logger');
+
+logger.header();
+
 switch (process.env.npm_lifecycle_event) {
 	case 'build:css':
 		require('./build/css')();
@@ -9,8 +13,14 @@ switch (process.env.npm_lifecycle_event) {
 		require('./build/css')(true);
 		break;
 
+	case 'build:js':
+		logger.message('Starting JS build...');
+		require('./build/js')();
+		break;
+
 	default:
-		console.log(`Unknown command: ${process.env.npm_lifecycle_event}`);
-		process.exit(1);
+		logger.exit(`Unknown command: ${process.env.npm_lifecycle_event}`, 1);
 		break;
 }
+
+logger.exit(`${process.env.npm_lifecycle_event} successfully completed :)`);
