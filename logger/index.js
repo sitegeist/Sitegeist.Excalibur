604eb840b75d4da21c913e1e38d05ba86d7fd134
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2017-05-17 10:01:05 +0200

    fix(Logger): Satisfy linter

--- logger/index.js
+++ logger/index.js
@@ -1,5 +1,5 @@
-const {version} = require('../package.json');
 const chalk = require('chalk');
+const {version} = require('../package.json');
 
 const now = () => {
 	const date = new Date();
@@ -10,14 +10,14 @@ const now = () => {
 module.exports.header = () => {
 	console.log('');
 	console.log(chalk.yellow.bgBlack('                                                                         '));
-    console.log(chalk.yellow.bgBlack('                  _ _                  _     _                           '));
-    console.log(chalk.yellow.bgBlack('        _     ___(_) |_ ___  __ _  ___(_)___| |_                         '));
-    console.log(chalk.yellow.bgBlack('      _| |_  / __| | __/ _ \\/ _\` |/ _ \\ / __| __|                        '));
-    console.log(chalk.yellow.bgBlack('     |_   _| \\__ \\ | ||  __/ (_| |  __/ \\__ \\ |_                         '));
-    console.log(chalk.yellow.bgBlack('       |_|   |___/_|\\__\\___|\\__, |\\___|_|___/\\__|                        '));
-    console.log(chalk.yellow.bgBlack('                            |___/                                        '));
+	console.log(chalk.yellow.bgBlack('                  _ _                  _     _                           '));
+	console.log(chalk.yellow.bgBlack('        _     ___(_) |_ ___  __ _  ___(_)___| |_                         '));
+	console.log(chalk.yellow.bgBlack('      _| |_  / __| | __/ _ \\/ _` |/ _ \\ / __| __|                       '));
+	console.log(chalk.yellow.bgBlack('     |_   _| \\__ \\ | ||  __/ (_| |  __/ \\__ \\ |_                         '));
+	console.log(chalk.yellow.bgBlack('       |_|   |___/_|\\__\\___|\\__, |\\___|_|___/\\__|                        '));
+	console.log(chalk.yellow.bgBlack('                            |___/                                        '));
 	console.log(chalk.yellow.bgBlack('                                                                         '));
-    console.log(chalk.yellow.bgBlack('                                    Neos Build Essentials ${version}     '));
+	console.log(chalk.yellow.bgBlack(`                                    Neos Build Essentials ${version}          `));
 	console.log(chalk.yellow.bgBlack('                                                                         '));
 
 	console.log('');
@@ -29,5 +29,5 @@ module.exports.message = (message, color = chalk.white) =>
 module.exports.exit = (message, exitCode = 0) => {
 	console.log(`${chalk.white(`[EXIT: ${now()}]`)} ${(exitCode === 0 ? chalk.green : chalk.red)(message)}`);
 	console.log('');
-	process.exit(exitCode);
+	process.exit(exitCode); // eslint-disable-line unicorn/no-process-exit
 };
