64bb99b1719ac6f1b0d291055646e2bb17ce7429
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2017-07-02 12:31:17 +0200

    FEATURE: Improve logger

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk');
 const padEnd = require('lodash.padend');
+const padStart = require('lodash.padstart');
 const {version} = require('../../package.json');
 
 const hangInThereMessages = [
@@ -19,8 +20,14 @@ const hangInThereMessages = [
 
 const now = () => {
 	const date = new Date();
+	const year = date.getFullYear();
+	const month = padStart(date.getMonth() + 1, 2, '0');
+	const day = padStart(date.getDay(), 2, '0');
+	const hours = padStart(date.getHours(), 2, '0');
+	const minutes = padStart(date.getMinutes(), 2, '0');
+	const seconds = padStart(date.getSeconds(), 2, '0');
 
-	return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDay()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+	return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 };
 
 module.exports.header = taskLabel => {
@@ -32,7 +39,8 @@ module.exports.header = taskLabel => {
 	console.log(chalk.yellow.bgBlack('            |_   _| \\__ \\ | ||  __/ (_| |  __/ \\__ \\ |_                  '));
 	console.log(chalk.yellow.bgBlack('              |_|   |___/_|\\__\\___|\\__, |\\___|_|___/\\__|                 '));
 	console.log(chalk.yellow.bgBlack('                                   |___/                                 '));
-	console.log(chalk.yellow.bgBlack(padEnd(`            EXCALIBUR ${version}                 Task: ${taskLabel}`, 73)));
+	console.log(chalk.yellow.bgBlack(padEnd(`           EXCALIBUR ⚔ ${version}                  Task: ${taskLabel}`, 73)));
+	console.log(chalk.yellow.bgBlack('                                                                         '));
 	console.log(chalk.yellow.bgBlack('                                                                         '));
 
 	console.log('');
