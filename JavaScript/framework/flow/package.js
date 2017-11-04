b8e63c674549171eb26967f43ddde3b6dc3cfa72
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2017-11-04 12:55:34 +0100

    FEATURE: Implement flow package manager

---
+++ JavaScript/framework/flow/package.js
@@ -0,0 +1,107 @@
+const path = require('path');
+const fs = require('fs-extra');
+const s = require('string');
+const glob = require('glob');
+
+module.exports = pathToPackage => {
+	const fileCache = {};
+	const fileAsJsonCache = {};
+	const componentNameCache = {};
+
+	const getFileContents = relativePathToFile => {
+		const absolutePathToFile = path.join(pathToPackage, relativePathToFile);
+
+		return fs.readFile(absolutePathToFile);
+	};
+
+	const Package = class {
+		constructor() {
+			this.getFileContents = async relativePathToFile => {
+				if (!fileCache[relativePathToFile]) {
+					fileCache[relativePathToFile] = getFileContents(relativePathToFile);
+				}
+				return fileCache[relativePathToFile];
+			};
+
+			this.getFileContentsAsJson = async relativePathToFile => {
+				if (!fileAsJsonCache[relativePathToFile]) {
+					fileAsJsonCache[relativePathToFile] = await this.getFileContents(relativePathToFile)
+						.then(contents => JSON.parse(contents));
+				}
+
+				return fileAsJsonCache[relativePathToFile];
+			};
+
+			this.getPrototypeNameFromFusionFilePath = async relativePathToFusionFile => {
+				if (!componentNameCache[relativePathToFusionFile]) {
+					if (relativePathToFusionFile === 'Resources/Private/Fusion/Root.fusion') {
+						return;
+					}
+
+					componentNameCache[relativePathToFusionFile] = `${await this.packageKey}:${
+						s(relativePathToFusionFile)
+							.chompLeft('Resources/Private/Fusion/')
+							.chompLeft('Resources/Private/TypoScript/')
+							.chompLeft('Integration/')
+							.chompLeft('Presentation/')
+							.chompRight('.fusion')
+							.chompRight('.ts2')
+							.replaceAll('/', '.')
+							.splitRight('.', 1)[0]
+					}`;
+				}
+
+				return componentNameCache[relativePathToFusionFile];
+			};
+
+			this.getJavaScriptFileNameFromFusionFilePath = async relativePathToFusionFile => {
+				return s(relativePathToFusionFile)
+					.chompRight('.fusion')
+					.chompRight('.ts2')
+					.toString()
+					.concat('.js');
+			};
+
+			this.createTemporaryFileName = fileName => {
+				return path.join(this.paths.temporary, fileName);
+			};
+
+			this.customize = async (configuration, scope) => {
+				console.log('TODO: Apply package customization.', {
+					scope
+				});
+
+				return configuration;
+			};
+		}
+
+		async initializeObject() {
+			const {rootPath} = await this.objectManager.get('context');
+
+			this.paths = {};
+			this.paths.root = pathToPackage;
+			this.paths.privateResources = path.join(this.paths.root, 'Resources/Private');
+			this.paths.publicResources = path.join(this.paths.root, 'Resources/Public');
+			this.paths.temporary = path.join(rootPath, 'Data/Temporary', await this.packageKey);
+		}
+
+		get composerName() {
+			return this.getFileContentsAsJson('composer.json').then(composerJson => {
+				return composerJson.name;
+			});
+		}
+
+		get packageKey() {
+			return this.getFileContentsAsJson('composer.json').then(composerJson => {
+				return composerJson.extra.neos['package-key'];
+			});
+		}
+
+		get fusionFiles() {
+			return glob.sync(`${this.paths.privateResources}/Fusion/**/*.{fusion,ts2}`)
+				.map(p => s(p).chompLeft(this.paths.root).chompLeft('/').toString());
+		}
+	};
+
+	return new Package();
+};
---
+++ JavaScript/framework/flow/packageManager.js
@@ -0,0 +1,73 @@
+const fs = require('fs-extra');
+const path = require('path');
+const git = require('simple-git/promise');
+
+const assertPathExists = async (basePath, subPath = '') => {
+	const targetPath = subPath ? path.join(basePath, subPath) : basePath;
+
+	if (!(await fs.pathExists(targetPath))) {
+		throw new Error(`Directory ${targetPath} does not exist!`);
+	}
+
+	return targetPath;
+};
+
+module.exports.singleton = async unassertedBaseDirectory => {
+	const packages = [];
+
+	const PackageManager = class {
+		constructor() {
+			this.get = async packageIdentifier => {
+				return (await Promise.all(packages.map(async flowPackage => ({
+					composerName: await flowPackage.composerName,
+					packageKey: await flowPackage.packageKey,
+					flowPackage
+				})))).filter(({composerName, packageKey}) => {
+					return (composerName === packageIdentifier) || (packageKey === packageIdentifier);
+				}).map(({flowPackage}) => flowPackage)[0];
+			};
+		}
+
+		async initializeObject() {
+			const manifest = await this.objectManager.get('manifest');
+			const unassertedBaseDirectory = (await this.objectManager.get('context')).rootPath;
+			const baseDirectory = await assertPathExists(unassertedBaseDirectory);
+			const gitInstance = git(baseDirectory);
+			const packagesDirectory = await assertPathExists(baseDirectory, 'Packages');
+			const packageTypes = (await fs.readdir(packagesDirectory))
+				.filter(d => !['..', '.', 'Libraries'].includes(d));
+
+			await Promise.all(packageTypes.map(async packageType => {
+				const packageTypeDirectory = await assertPathExists(packagesDirectory, packageType);
+				const packageKeys = (await fs.readdir(packageTypeDirectory))
+					.filter(d => !['..', '.'].includes(d));
+
+				return Promise.all(packageKeys.map(async packageKey => {
+					const pathToComposerJson = path.join(packageTypeDirectory, packageKey, 'composer.json');
+					const packageIsIncluded = (
+						manifest.isPackageIncluded(packageKey) &&
+						(await fs.pathExists(pathToComposerJson)) &&
+						!(await gitInstance.checkIgnore(pathToComposerJson)).length
+					);
+
+					if (packageIsIncluded) {
+						const pathToPackage = path.join(packageTypeDirectory, packageKey);
+						const flowPackage = await this.objectManager.get('flow/package', pathToPackage);
+
+						packages.push(flowPackage);
+					}
+				}));
+			}));
+		}
+
+		forEach(...args) {
+			packages.forEach(...args);
+		}
+
+		map(...args) {
+			return packages.map(...args);
+		}
+	};
+
+	return new PackageManager();
+};
