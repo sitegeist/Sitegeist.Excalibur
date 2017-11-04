922f80392f216baf33e8da6cd2ca5c5d2a9e4785
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2017-11-04 15:12:59 +0100

    FEATURE: Improve performance of js build

--- JavaScript/tasks/build-js/index.js
+++ JavaScript/tasks/build-js/index.js
@@ -1,8 +1,16 @@
 const path = require('path');
+const fs = require('fs-extra');
 const webpack = require('webpack');
+const chokidar = require('chokidar');
 const {$all, $set, $add} = require('plow-js');
 
-const GenerateEntryFilePlugin = require('./webpackPlugins/generateEntryFile');
+const entryFileTemplate = components => `
+require('sitegeist-excalibur/JavaScript/runtime')({
+${components.map(component => `
+	'${component.prototypeName}': require('${component.javascriptFile}')
+`)}
+});
+`.trim();
 
 module.exports = class {
 	get id() {
@@ -27,6 +35,26 @@ module.exports = class {
 
 			this.continue();
 		};
+
+		this.generateEntryFile = async () => {
+			const fusionFiles = await this.flowPackage.fusionFiles;
+			const javascriptComponentMap = (await Promise.all(fusionFiles.map(async fusionFile => {
+				const [prototypeName, javascriptFile] = await Promise.all([
+					await this.flowPackage.getPrototypeNameFromFusionFilePath(fusionFile),
+					await this.flowPackage.getJavaScriptFileNameFromFusionFilePath(fusionFile)
+				]);
+
+				if (await fs.pathExists(path.join(this.flowPackage.paths.root, javascriptFile))) {
+					return {
+						prototypeName,
+						javascriptFile: path.join(this.flowPackage.paths.root, javascriptFile)
+					};
+				}
+			}))).filter(i => i);
+
+			await fs.ensureFile(this.entryFileName);
+			await fs.writeFile(this.entryFileName, entryFileTemplate(javascriptComponentMap));
+		};
 	}
 
 	async configure(flowPackage, manifest) {
@@ -48,11 +76,6 @@ module.exports = class {
 			path: path.join(await flowPackage.paths.publicResources, 'JavaScript')
 		}));
 
-		webpackConfiguration.append('excalibur/entry-file-plugin', $add(
-			'plugins',
-			new GenerateEntryFilePlugin(flowPackage, entryFileName)
-		));
-
 		webpackConfiguration.append('excalibur/babel-loader', $add('module.rules', {
 			test: /\.js$/,
 			use: 'babel-loader'
@@ -63,13 +86,52 @@ module.exports = class {
 
 		this.compiler = webpack(webpackConfiguration.configuration);
 		this.packageKey = await flowPackage.packageKey;
+		this.javascriptFilePattern = path.join(flowPackage.paths.privateResources, 'Fusion/**/*.js');
+		this.entryFileName = entryFileName;
+		this.flowPackage = flowPackage;
 	}
 
 	async run() {
+		//
+		// Generate the entry file
+		//
+		await this.generateEntryFile();
+
+		//
+		// Run the webpack build
+		//
 		this.compiler.run(this.build);
 	}
 
 	async watch() {
+		//
+		// Webpack has an initial 10 second delay when watching files. Since we're generating an entry file right
+		// before the watcher is started, we're getting stuck in a loop and webpack is compiling the bundle over
+		// and over again for 10 seconds.
+		//
+		// So, here's the workaround as per https://github.com/webpack/watchpack/issues/25#issuecomment-319292564
+		//
+		const timefix = 11000;
+		this.compiler.plugin('watch-run', (watching, callback) => {
+			watching.startTime += timefix;
+			callback();
+		});
+		this.compiler.plugin('done', stats => {
+			stats.startTime -= timefix;
+		});
+
+		//
+		// Generate the entry file and listen for changes to regenerate the entry file, whenever a JavaScript file
+		// is added or removed
+		//
+		await this.generateEntryFile();
+		chokidar.watch(this.javascriptFilePattern, {ignoreInitial: true})
+			.on('add', () => this.generateEntryFile())
+			.on('unlink', () => this.generateEntryFile());
+
+		//
+		// Run the webpack watcher
+		//
 		this.compiler.watch({}, this.build);
 	}
 };
