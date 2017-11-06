b7db4d3d5868e6a0e3881ea8fab076b93849ad82
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2017-11-06 16:41:32 +0100

    TASK: Re-implement header

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+const chalk = require('chalk');
+const pad = require('lodash.pad');
+
+const {version} = require('../package.json');
 const logger = require('./src/logger');
 const packageJson = require(process.cwd() + '/package.json');
 const createObjectManager = require('./framework');
@@ -6,6 +10,23 @@ const createObjectManager = require('./framework');
 const {argv} = require('yargs')
 	.count('verbose')
 	.alias('v', 'verbose');
+
+console.log('');
+console.log(chalk.yellow.bgBlack('                                                                                                                        '));
+console.log(chalk.yellow.bgBlack('                                                                                                                        '));
+console.log(chalk.yellow.bgBlack('                                                 _ _                  _     _                                           '));
+console.log(chalk.yellow.bgBlack('                                       _     ___(_) |_ ___  __ _  ___(_)___| |_                                         '));
+console.log(chalk.yellow.bgBlack('                                     _| |_  / __| | __/ _ \\/ _` |/ _ \\ / __| __|                                        '));
+console.log(chalk.yellow.bgBlack('                                    |_   _| \\__ \\ | ||  __/ (_| |  __/ \\__ \\ |_                                         '));
+console.log(chalk.yellow.bgBlack('                                      |_|   |___/_|\\__\\___|\\__, |\\___|_|___/\\__|                                        '));
+console.log(chalk.yellow.bgBlack('                                                           |___/                                                        '));
+console.log(chalk.yellow.bgBlack('                                                                                                                        '));
+console.log(chalk.yellow.bgBlack(pad(`EXCALIBUR ⚔ ${version}`, 120)));
+console.log(chalk.yellow.bgBlack('                                                                                                                        '));
+console.log(chalk.yellow.bgBlack('                                                                                                                        '));
+console.log(chalk.yellow.bgBlack('                                                                                                                        '));
+
+console.log('');
 
 const app = async () => {
 	try {
