bf0849647909b463f7600316b66397fefeb326f3
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2017-11-07 12:51:30 +0100

    TASK: Completely remove old sources

--- JavaScript/src/service/resolve-package-information.js
+++
@@ -1,5 +0,0 @@
-const executeFlowCommand = require('./execute-flow-command');
-
-module.exports = async (logger, error) => JSON.parse(
-	await executeFlowCommand(logger, error, 'build:printpackageinformation')
-);
--- JavaScript/src/service/resolve-style-settings.js
+++
@@ -1,6 +0,0 @@
-const executeFlowCommand = require('./execute-flow-command');
-
-module.exports = (logger, error, sitePackageKey) =>
-	executeFlowCommand(logger, error, 'build:printstylesettingsforsitepackage', {
-		sitePackageKey
-	});
