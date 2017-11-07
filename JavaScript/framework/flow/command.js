a333419af72cbd877ee2847f80e10497f21ca96d
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2017-11-07 10:31:20 +0100

    FEATURE: Implement flow command abstraction

---
+++ JavaScript/framework/flow/command.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const fs = require('fs-extra');
+const paramCase = require('param-case');
+const {spawn} = require('child-process-promise');
+
+module.exports.singleton = () => {
+	const Command = class {
+		async initializeObject() {
+			const {rootPath} = await this.objectManager.get('context');
+
+			this.logger = this.logger || (await this.objectManager.get('logger'));
+			this.flowExecutable = path.join(rootPath, 'flow');
+			this.rootPath = rootPath;
+
+			if (!(await fs.pathExists(this.flowExecutable))) {
+				this.logger.error(`Could not find flow executable at "${this.flowExecutable}"`);
+				process.exit(1);
+			}
+		}
+
+		async run(command, options = {}) {
+			const commandString = `${this.flowExecutable} ${command} ${Object.keys(options).map(
+				key => ` --${paramCase(key)}="${options[key]}"`
+			)}`;
+			const childProcess = spawn(this.flowExecutable, [command, ...Object.keys(options).map(
+				key => ` --${paramCase(key)}="${options[key]}"`
+			)], {capture: ['stdout']});
+
+			this.logger.debug(`Executing "${commandString.replace(this.rootPath, '.')}".`, 2);
+
+			childProcess.childProcess.stdout.on('data', data => {
+				this.logger.debug(`"${commandString.replace(this.rootPath, '.')}":`, 3);
+				this.logger.debug(data.toString(), 3);
+			});
+
+			childProcess.childProcess.stderr.on('data', data => {
+				this.logger.error(`Error while executing "${commandString.replace(this.rootPath, '.')}":`);
+				this.logger.error(data.toString());
+			});
+
+			const result = await childProcess;
+
+			return result.stdout.toString();
+		}
+
+		async getInstance(logger) {
+			const newInstance = new Command();
+
+			newInstance.objectManager = this.objectManager;
+			newInstance.logger = logger || this.logger;
+
+			return newInstance;
+		}
+	};
+
+	return new Command();
+};
