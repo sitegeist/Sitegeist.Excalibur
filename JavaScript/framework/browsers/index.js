a0c5f7ee21ee4878534412bd54467c8fb94ab7e6
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2017-12-08 13:44:45 +0100

    TASK: Add logger to browsers

--- JavaScript/framework/browsers/index.js
+++ JavaScript/framework/browsers/index.js
@@ -3,10 +3,19 @@ const browserslist = require('browserslist');
 module.exports.singleton = () => {
 	const browsers = browserslist();
 	const Browsers = class {
+		async initializeObject() {
+			this.logger = await (await this.objectManager.get('logger')).createInstance('browsers');
+		}
+
 		compare(query) {
 			const browsersToCompareWith = browserslist(query);
+			const result = browsersToCompareWith.some(browser => browsers.includes(browser));
 
-			return browsersToCompareWith.some(browser => browsers.includes(browser));
+			if (result) {
+				this.logger.debug(`${query} matches`, 2);
+			}
+
+			return result;
 		}
 	};
 
